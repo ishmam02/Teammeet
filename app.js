3be75243971464728a2c492080e4e36a35f14bd5
ishmamKabir <kabirishmam@gmail.com>
2021-03-29 19:58:26 +0600

    Test

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import 'express-async-errors';
 import bodyparser from 'body-parser';
 import cookieSession from 'cookie-session';
 import { currentUser, errorHandler, NotFoundError } from '@ishmam_tech/common';
-import path from 'path';
+
 import { currentUserRouter } from './routes/current-user.js';
 import { authRouter } from './routes/auth.js';
 import PeerServer from './services/PeerServer.js';
@@ -31,18 +31,9 @@ app.use(currentUserRouter);
 app.use(authRouter);
 new PeerServer(server, app).attach();
 new SocketServer(server).attach();
-
-if (process.env.NODE_ENV === 'production') {
-	// Express will serve up production assets
-	// like our main.js file, or main.css file!
-	app.use(express.static('client/build'));
-
-	// Express will serve up the index.html file
-	// if it doesn't recognize the route
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-	});
-}
+app.all('*', async (_req, _res) => {
+	throw new NotFoundError();
+});
 
 app.use(errorHandler);
 
